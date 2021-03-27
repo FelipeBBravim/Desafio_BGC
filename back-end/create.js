58dbfd794983543af4750b4b6c69aa855e0edb1a
FelipeBBravim <felipe.bravim@gmail.com>
2021-03-27 20:11:42 -0300

    Adicionando item com o 'post'

---
+++ back-end/create.js
@@ -0,0 +1,27 @@
+//import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.TABLE_PRODUTOS,
+        Item: {
+            // The attributes of the item to be created
+            produtoId: Math.floor(Math.random() * 1000),        // The id of the author
+            nome: data.nome,            // A unique uuid
+            descricao: data.descricao,      // Parsed from request body
+            tamanho: data.tamanho,// Parsed from request body
+            nomeMinion: data.nomeMinion,      // Current Unix timestamp
+            material: data.material,
+            preco: data.preco,
+        },
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
+
+});
