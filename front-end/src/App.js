f0c7c4ae9d39d21bead39273cb9bf33f0a19384a
FelipeBBravim <felipe.bravim@gmail.com>
2021-03-30 20:50:37 -0300

    Adicionando Router e LinkContainer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Nav } from "react-bootstrap";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Loja Minions BGC
-        </Navbar.Brand>
+        <LinkContainer to="/">  
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Loja Minions BGC
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
